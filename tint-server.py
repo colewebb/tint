e8e5c88aa76f4bd56f7b0ccdbbe6a753c2e40fd2
Cole Webb <cole.webb@outlook.com>
2017-06-25 22:26:57 -0700

    added logic, infinite loop for handling user input, seperate script handling server functions, interactive abilities, a crap ton of other stuff.

--- tint-server.py
+++ tint-server.py
@@ -1,4 +1,13 @@
+# imports, startup message
+
 import socket
-from subprocess import Popen
-p=Popen("ping -c 2 scanme.nmap.org",shell=True)
-print(p)
+from platform import system
+system_type=system().lower()
+print("Welcome to Tint server 0.0.1 (pre-alpha) on "+system_type+". Read the docs, dumbo.")
+
+# start listening on port 51674
+
+print("listening on port 51674")
+receive=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+receive.bind(('',51674))
+receive.listen(5)
--- tint.py
+++ tint.py
@@ -1,29 +1,48 @@
-# imports, startup message
+# imports, startup message, other startup
+
 import socket
 from platform import system
-from subprocess import call
+from subprocess import call, Popen
 system_type=system().lower()
 print("Welcome to Tint 0.0.1 (pre-alpha) on "+system_type+". Read the docs, dumbo.")
-# start listening on port 51674
-receive=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-receive.bind(('',51674))
-receive.listen(5)
+
+#function definitions
+
+def find_peers():
+	print("Using nmap to find potential peers on the network, please wait...")
+	call("nmap -p 51674 192.168.0.0/24 | grep 'Nmap scan report' > /home/rebooted/scripts/tint/hosts.txt",shell=True)
+	print("nmap host discovery completed. moving on to connection testing...")
+	peers=open("/home/rebooted/scripts/tint/hosts.txt")
+	potential_peer_list=[]
+	peer_list=[]
+	for line in peers:
+		potential_peer_list.append(line[21:].rstrip())
+	for peer in potential_peer_list:
+		try:
+			send.connect((peer,51674))
+			print("Peer found at "+peer+".")
+			peer_list.append(peer)
+		except:
+			print("No host found on "+peer+".")
+	return peer_list
+
 # setup client socket
+
 send=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 send.settimeout(5)
-# scan network for that port on other devices
-i=0
-while i <=264:
-	if system_type == "windows":
-		stuff_to_call="ping -n 1 192.168.0."+str(i)
-		result=call(stuff_to_call,shell=True)
-		print(result)
+
+command=raw_input("tint >>> ")
+while True:
+	if command=="x":
+		exit()
+	elif command=="exit":
+		exit()
+	elif command=="scan":
+		peers=find_peers()
+		print(peers)
 	else:
-		stuff_to_call=call("ping -c 1 192.168.0."+str(i)
-		result=call(stuff_to_call,shell=True)
-		print(result)
-# wait for permission to exit...
-raw_input("waiting, press enter to exit...")
+		print("Command '"+command+"' is not found. Try:\n\n - scan: scan network for peers\n - send: send a file to a peer\n - exit: exit the program\n")
+	command=raw_input("tint >>> ")
 # return list of ip and mac addresses, and aliases from configuration file (if available)
 
 # offer prompt
@@ -37,4 +56,5 @@ raw_input("waiting, press enter to exit...")
 # send: use tint protocol to send file, including encryption key exchange, file information, confirmation, etc.
 
 # exit: notify other computers of exit, save files, exit
-receive.close()
+
+send.close()
